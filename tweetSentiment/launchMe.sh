b99bdfb16e5bb283eb3add4596116c2673e6fa75
LoreBerli <mrgorsky91@gmail.com>
2017-02-23 12:04:30 +0100

    updated bash script to support topics

--- tweetSentiment/launchMe.sh
+++ tweetSentiment/launchMe.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-classifierPath=
+cPATH=
+
 mapper=
+filter=
 reducer=
 hadp=
 streamingApi=
 inputPath=
 outputPath=
-##########################################
+topics=london madrid moscow paris rome
 
+###################################################################
 
-$hadp jar $streamingApi -mapper $mapper -reducer $reducer -input $inputPath -output $outputPath -file $mapper -file $reducer -file $classifierPath
+$hadp jar $streamingApi -file $mapper -file $reducer -file $cPATH -mapper $filter  -reducer $reducer -input $inputPath -output $outputPath
